708392782fcb93830101ab3a6238788d0a7fac8e
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2021-05-01 19:29:15 +0900

    #02 Tailwind CSS Setup

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+//postcss.config.js 파일은 tailwind를 일반 css파일로 빌드하기위해 필요한 파일
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+//tailwind.config.js 파일은 tailwind를 커스텀하기 위해 필요한 파일
+//npx tailwindcss init 하면 생성되는 파일
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
